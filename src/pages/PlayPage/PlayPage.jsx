8ad48a67fff56c817ade2ab37ca6c55c73f82215
seant3 <116916697+seant3@users.noreply.github.com>
2023-02-02 14:36:40 -0500

    question and answers are rendering

--- src/pages/PlayPage/PlayPage.jsx
+++ src/pages/PlayPage/PlayPage.jsx
@@ -4,11 +4,15 @@ import triviaApi from "../../utils/triviaApi";
 
 export default function PlayPage() {
     const [question, setQuestion] = useState([])
+    const [correctAnswer, setCorrectAnswer] = useState("");
+    const [incorrectAnswers, setIncorrectAnswers] = useState([])
 
     async function getQuestions() {
         const response = await triviaApi.getQuestions();
         console.log(response.results, "this is the response from getQuestions")
         setQuestion(response.results[0].question)
+        setCorrectAnswer(response.results[0].correct_answer)
+        setIncorrectAnswers(response.results[0].incorrect_answers)
     }
 
     useEffect(() => {
@@ -17,6 +21,10 @@ export default function PlayPage() {
     }, []);
 
     return (
-        <div>{question}</div>
+        <>
+            <div>{question}</div>
+            <div>{correctAnswer + ","}{incorrectAnswers + " "}</div>
+            <div></div>
+        </>
     )
 }
