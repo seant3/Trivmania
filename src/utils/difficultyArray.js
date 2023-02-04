c8555fa04c63a4ce7c8512cdb6d1ccc8066b68a0
seant3 <116916697+seant3@users.noreply.github.com>
2023-02-04 13:43:11 -0500

    added category and difficulty arrays from the API documentation

---
+++ src/utils/difficultyArray.js
@@ -0,0 +1,24 @@
+const Difficulty = [
+    {
+        key: '0',
+        value: '0',
+        text: "Any Difficulty"
+    },
+    {
+        key: 'easy',
+        value: 'easy',
+        text: "Easy"
+    },
+    {
+        key: 'medium',
+        value: 'medium',
+        text: "Medium"
+    },
+    {
+        key: 'hard',
+        value: 'hard',
+        text: "Hard"
+    }
+]
+
+export default Difficulty;
