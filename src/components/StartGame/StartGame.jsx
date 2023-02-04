d0f653eeb9c5f93933b4dc929047688c966a1c36
seant3 <116916697+seant3@users.noreply.github.com>
2023-02-04 13:07:39 -0500

    refactored PlayPage to make category/difficulty choices capable

--- src/components/StartGame/StartGame.jsx
+++ src/components/StartGame/StartGame.jsx
@@ -1,9 +1,93 @@
+import { useState, useEffect } from "react";
+import shuffleService from "../../utils/shuffleService";
+import AnswerDisplay from "../../components/AnswerDisplay/AnswerDisplay";
+import { Container, Button, Menu, Item, Header, Segment, Grid, Message } from "semantic-ui-react";
 
+import he from 'he';
 
-export default function StartGame({startGame}) {
-    
-    
+export default function StartGame({data}) {
+    const [question, setQuestion] = useState([]);
+    const [correctAnswer, setCorrectAnswer] = useState("");
+    const [allChoices, setAllChoices] = useState([]);
+    const [questionNum, setQuestionNum] = useState(0);
+    const [points, setPoints] = useState(0);
+    const [isCorrect, setIsCorrect] = useState({answer: ""})
+    // const [isPlaying, setIsPlaying] = useState(false)
+
+    function getQAndA() {
+        
+        
+        setQuestion(he.decode(data[questionNum].question))
+        setCorrectAnswer(data[questionNum].correct_answer)
+        
+        let answers = [];
+        data[questionNum].incorrect_answers.map((incorrectAnswer) => {
+            answers.push(he.decode(incorrectAnswer))  
+        });
+        answers.push(he.decode(data[questionNum].correct_answer))
+        const shuffledAnswers = shuffleService.shuffle(answers)
+
+        setAllChoices(shuffledAnswers)
+        // setIsPlaying(true)
+
+    }
+
+    function verifyAnswer(e, { name }) {
+        console.log(name, "name")
+        if (name === correctAnswer) {
+            setIsCorrect({
+                answer: correctAnswer
+            })
+            setPoints(points + 1);
+            console.log(e.target, "e.target =======")
+        }
+        setIsCorrect({
+            answer: correctAnswer
+        })
+    }
+
+    function nextQuestion() {
+        setIsCorrect({
+            answer: ""
+        })
+        setQuestionNum((questionNum) => questionNum +1);
+    }
+
+    useEffect(() => {
+        if(data.length > 0) {
+            getQAndA()
+        }
+    }, [questionNum])
+
     return (
-        <button onClick={startGame}>Start Game</button>
+        <>
+        
+                <Grid centered>
+                    <Grid.Column>
+                        <Header>
+                            {`Question #${questionNum + 1}`}<br></br>
+                            <span>Points:{points}</span>
+                        </Header>
+                        <Segment>
+                            {question}
+                            <Menu fluid pointing vertical>
+                                <AnswerDisplay allChoices={allChoices} verifyAnswer={verifyAnswer} isCorrect={isCorrect}/>
+                            </Menu>
+                        </Segment>
+                                
+                        <Segment>
+                        <Button onClick={nextQuestion} type="submit" className="btn" size="large" fluid>
+                            Next
+                        </Button> 
+                        </Segment>
+                    </Grid.Column>
+                </Grid>
+        
+    
+                
+            
+            
+        </>
+        
     )
 }
--- src/pages/PlayPage/PlayPage.jsx
+++ src/pages/PlayPage/PlayPage.jsx
@@ -1,105 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import { Container, Button, Menu, Item, Header, Segment, Grid, Message } from "semantic-ui-react";
-import shuffleService from "../../utils/shuffleService";
-import AnswerDisplay from "../../components/AnswerDisplay/AnswerDisplay";
+
 import StartGame from "../../components/StartGame/StartGame";
 
 import triviaApi from "../../utils/triviaApi";
 
-import he from 'he';
-
 export default function PlayPage() {
-    const [question, setQuestion] = useState([]);
-    const [correctAnswer, setCorrectAnswer] = useState("");
-    const [allChoices, setAllChoices] = useState([]);
-    const [questionNum, setQuestionNum] = useState(0);
-    const [points, setPoints] = useState(0);
+    
     const [data, setData] = useState([])
     const [isPlaying, setIsPlaying] = useState(false)
-    const [isCorrect, setIsCorrect] = useState({answer: ""})
+    
 
     async function getData() {
         const response = await triviaApi.getQuestions();
         console.log(response.results, "this is the response from getQuestions")
         setData(response.results);
-    }
-
-
-    function getQAndA() {
-        
-        
-        setQuestion(he.decode(data[questionNum].question))
-        setCorrectAnswer(data[questionNum].correct_answer)
-        
-        let answers = [];
-        data[questionNum].incorrect_answers.map((incorrectAnswer) => {
-            answers.push(he.decode(incorrectAnswer))  
-        });
-        answers.push(he.decode(data[questionNum].correct_answer))
-        const shuffledAnswers = shuffleService.shuffle(answers)
-
-        setAllChoices(shuffledAnswers)
         setIsPlaying(true)
-
-    }
-
-
-
-    useEffect(() => {
-        getData();
-    }, []);
-
-
-    function verifyAnswer(e, { name }) {
-        console.log(name, "name")
-        if (name === correctAnswer) {
-            setIsCorrect({
-                answer: correctAnswer
-            })
-            setPoints(points + 1);
-            console.log(e.target, "e.target =======")
-        }
-        setIsCorrect({
-            answer: correctAnswer
-        })
-    }
-
-    function nextQuestion() {
-        setIsCorrect({
-            answer: ""
-        })
-        setQuestionNum((questionNum) => questionNum +1);
-    }
-
-    useEffect(() => {
-        if(data.length > 0) {
-            getQAndA()
-        }
-    }, [questionNum])
+    }  
 
     return (
-        
-        <Grid centered>
-            <Grid.Column>
-            <Header>
-                {`Question #${questionNum + 1}`}<br></br>
-                <span>Points:{points}</span>
-            </Header>
-            <Segment>
-                {question}
-                 <Menu fluid pointing vertical>
-                     <AnswerDisplay allChoices={allChoices} verifyAnswer={verifyAnswer} isCorrect={isCorrect}/>
-                </Menu>
-            </Segment>
-                    
-            <Segment>
-            {isPlaying ? <Button onClick={nextQuestion} type="submit" className="btn" size="large" fluid>
-                    Next
-                </Button> : <StartGame startGame={getQAndA}/>}
-            </Segment>
-            </Grid.Column>
-        </Grid>
+       <>
+        {isPlaying ? 
+            <StartGame data={data} isPlaying={isPlaying}/> :
+            <button onClick={getData}>Start Game</button>
+        }   
+    </>
         
     )
 }
